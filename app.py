e66563a2edb6713a7f864f345b9f71df8638f8aa
gael <gael.bernard@epfl.ch>
2023-06-21 14:35:11 +0200

    dd

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 import sqlite3
-from flask import Flask, redirect, render_template, request, send_from_directory, url_for, send_static_file
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 from flask_cors import CORS
 
 #ENTRYPOINT ["gunicorn", "app:app", "--bind", "0.0.0.0:5008", "--timeout", "10000"]
